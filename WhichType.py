1187b81598d5e00b92943af5142ead2133f5b6aa
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-16 10:03:33 +0530

    commit

---
+++ WhichType.py
@@ -0,0 +1,11 @@
+#you can't any chamge in tupple
+a =(1,45,342,34,3424,False,"Rohan","shivam",34)
+print (a)
+print (type(a))
+no =a.count(45)
+print (no)
+
+#count the index value only one time
+i =a.index(34)
+print (i)
+print (len(a))
