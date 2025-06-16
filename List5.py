cbfa97800044b3c996da1f8c4ffe209a4286e7f9
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-16 09:21:20 +0530

    Commit

---
+++ List5.py
@@ -0,0 +1,12 @@
+friends =["Apple","Orange",5,345.06,False,"Aakash","Rohan"]
+print (friends)
+friends.append("Mini")
+print(friends) 
+
+l1 =[1,34,62,2,6,11]
+#l1.sort()
+#l1.reverse() #start to end 
+#l1.insert(2,333333)#Insert 333333 such that its index in the list  is3
+print(l1.pop(3))#same as value =l1.pop(3)
+                #print(value)
+print(l1)
