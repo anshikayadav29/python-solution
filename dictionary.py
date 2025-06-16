9ae14f7a2fbdf28653b9ab633de626d461dab04e
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-16 12:14:58 +0530

    commit

--- dictionary.py
+++ dictionary.py
@@ -4,4 +4,10 @@ marks ={
     "Rohan":23
 }
 #print(marks,type(marks))
-print(marks["Mini"])
+#print(marks["Mini"])
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Anamika":88,"Renuka":100})
+print (marks)
+print(marks.get("Anamika"))
