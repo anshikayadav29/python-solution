ea76e8bdd859feb0ebdbe61000ed48e5dd7c8295
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-18 14:51:01 +0530

    commit

--- update.py
+++ update.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import sqlite3
-conn=sqlite3.connect("sqlite.db")
+conn = sqlite3.connect("sqlite.db")
 conn.execute('''
       update student set st_name='ravi',st_class='10th'where st_id=1
 
@@ -9,16 +8,4 @@ conn.execute('''
 
 ''')
 conn.commit()
-=======
-import sqlite3
-conn=sqlite3.connect("sqlite.db")
-conn.execute('''
-      update student set st_name='ravi',st_class='10th'where st_id=1
 
-
-
-
-''')
-conn.commit()
->>>>>>> ab583f43d8413cdd27b4721a6660931f241fee07
-conn.close()
