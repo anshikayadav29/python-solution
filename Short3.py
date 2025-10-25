a070358b138e2519dd80fa9b8b92048f932b5fac
anshikayadav29 <anshikayadav2917@gmail.com>
2025-10-25 10:37:15 +0530

    Commit

---
+++ Short3.py
@@ -0,0 +1,6 @@
+arr = [5, 2, 9, 1, 7]
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] > arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
+print("Sorted Array:", arr)
