5d2d302950428151587a5187e498e799fd4d46c2
anshikayadav29 <anshikayadav2917@gmail.com>
2026-02-21 00:40:16 +0530

    Create Metaclasses.js

---
+++ Metaclasses.js
@@ -0,0 +1,9 @@
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Database(metaclass=Singleton):
+    pass # No matter how many times you call Database(), it's the same object.
